209fbc8bf38bab03b5859ce810290e65e59ee069
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-03-26 00:18:45 +0300

    fix #2, changing language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def default_url_options(options = {})
+    if I18n.locale == I18n.default_locale
+      { locale: nil }.merge options
+    else
+      { locale: I18n.locale }.merge options
+    end
+  end
+
   protected
 
   def set_locale
--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -1,20 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe IndexController, type: :controller do
-  before(:each) { expect(controller).to receive(:set_locale) }
+  before(:each) { allow(controller).to receive(:set_locale) }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
+  shared_examples 'setting locale' do
+    it 'should call set_locale' do
+      expect(controller).to have_received(:set_locale)
+    end
+  end
+
+  shared_examples 'successful response' do
+    it 'returns HTTP success' do
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe "GET #index" do
+    before(:each) { get :index }
+
+    it_should_behave_like 'successful response'
+    it_should_behave_like 'setting locale'
+  end
+
   describe "GET #about" do
-    it "returns http success" do
-      get :about
-      expect(response).to have_http_status(:success)
-    end
+    before(:each) { get :about }
+
+    it_should_behave_like 'successful response'
+    it_should_behave_like 'setting locale'
   end
 
 end
