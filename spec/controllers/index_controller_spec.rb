e3faea8b50354d06b1cf3517cf3297d4e0066312
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-03-25 14:25:50 +0300

    see #2, setting locale

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe IndexController, type: :controller do
+  before(:each) { expect(controller).to receive(:set_locale) }
 
   describe "GET #index" do
     it "returns http success" do
