7137f628e5f7d2938aa72f9ab8891d20560b8b5a
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-03-28 16:01:24 +0300

    see #4, uploader and locale files for AR

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ActiveRecord::Base
   validates_presence_of :slug, :name_ru, :name_en, :name_es
   validates_uniqueness_of :slug, :name_ru, :name_en, :name_es
 
+  mount_uploader :image, HeadingUploader
+
   enum visibility: [:hidden, :visible, :main]
 
   def self.for_anonymous
