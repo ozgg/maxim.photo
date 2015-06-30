bcb48743d1df1868f11e47e3d214e03b6e0083d7
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-07-01 02:41:30 +0300

    Main layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_page
+  helper_method :current_user, :current_page
+
+  # Get current user from session
+  #
+  # @return [User]
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) unless session[:user_id].nil?
+  end
 
   # Get current page from request
   #
