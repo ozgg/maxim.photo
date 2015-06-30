4b949695bdef8843934e383addeb0d9e49673dcb
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-07-01 02:04:19 +0300

    Dimensions for ImageUploader

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,7 @@
 class Photo < ActiveRecord::Base
+  validates_presence_of :name, :image
+
+  mount_uploader :image, ImageUploader
+
   scope :visible, -> { where(visible: true) }
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,14 +1,10 @@
 # encoding: utf-8
 
 class ImageUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -31,21 +27,35 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :big do
+    process resize_to_fit: [1280, 720]
+  end
+
+  version :medium, from_version: :big do
+    process resize_to_fit: [640, 360]
+  end
+
+  version :big_square, from_version: :big do
+    process resize_to_fill: [600, 600]
+  end
+
+  version :medium_suqare, from_version: :big_square do
+    process resize_to_fit: [300, 300]
+  end
+
+  version :small_square, from_version: :medium_square do
+    process resize_to_fit: [150, 150]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
