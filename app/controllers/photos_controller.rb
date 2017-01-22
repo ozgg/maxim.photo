26932e390d69dc40e2ea2a19a50d175a65d7affc
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-01-23 01:29:43 +0300

    Form for photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,62 @@
 class PhotosController < ApplicationController
+  before_action :restrict_anonymous_access, except: [:index, :show]
+  before_action :set_entity, only: [:edit, :update, :destroy]
+
+  # get /photos
+  def index
+    @collection = Photo.page_for_visitors(current_page)
+  end
+  
+  # get /photos/new
+  def new
+    @entity = Photo.new
+  end
+
+  # post /photos
+  def create
+    @entity = Photo.new entity_parameters
+    if @entity.save
+      redirect_to admin_photo_path(@entity)
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  # get /photos/:id
+  def show
+  end
+
+  # get /photos/:id/edit
+  def edit
+  end
+
+  # patch /photos/:id
+  def update
+    if @entity.update entity_parameters
+      redirect_to admin_photo_path(@entity), notice: t('photos.update.success')
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  # delete /photos/:id
+  def destroy
+    if @entity.destroy
+      flash[:notice] = t('photos.destroy.success')
+    end
+    redirect_to admin_photos_path
+  end
+
+  protected
+
+  def set_entity
+    @entity = Photo.find_by(id: params[:id])
+    if @entity.nil?
+      handle_http_404('Cannot find photo')
+    end
+  end
+
+  def entity_parameters
+    params.require(:photo).permit(Photo.entity_parameters)
+  end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,29 @@
 class Photo < ApplicationRecord
+  TITLE_LIMIT    = 50
+  KEYWORDS_LIMIT = 250
+  LEAD_LIMIT     = 250
+  PER_PAGE       = 12
+
   mount_uploader :image, PhotoImageUploader
+
+  validates_presence_of :title, :image
+  validates_length_of :title, maximum: TITLE_LIMIT
+  validates_length_of :lead, maximum: LEAD_LIMIT
+  validates_length_of :keywords, maximum: KEYWORDS_LIMIT
+
+  scope :recent, -> { order('id desc') }
+
+  # @param [Integer] page
+  def self.page_for_administration(page = 1)
+    recent.page(page).per(PER_PAGE)
+  end
+
+  # @param [Integer] page
+  def self.page_for_visitors(page = 1)
+    recent.page(page).per(PER_PAGE)
+  end
+
+  def self.entity_parameters
+    %i(image title hazard_motion nudity keywords lead description shot_date)
+  end
 end
